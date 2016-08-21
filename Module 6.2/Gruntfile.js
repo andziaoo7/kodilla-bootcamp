e2017210c66f8c85abc0d93281ce708f1d777ef1
andziaoo7 <a.kalamacka@gmail.com>
2016-08-21 16:41:00 +0200

    Gruntfile

---
+++ Module 6.2/Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'style.css': 'style.sass'
+        }
+      }
+    },
+    imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'images/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'images/build/'
+        }]
+      }
+    },
+    watch: {
+    scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        },
+      }
+    },
+    connect: {
+        server: {
+          options: {
+          hostname: "127.0.0.1",
+          port: 9001
+        }
+      }
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : [
+                  '*.css',
+                  '*.html'
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: "./"
+          }
+        }
+      }
+    }
+  });
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'imagemin', 'connect', 'browserSync']);
+};
