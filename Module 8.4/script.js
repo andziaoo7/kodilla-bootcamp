4742260f593d56444ddaf3471c8dba5d4b39c4ae
andziaoo7 <a.kalamacka@gmail.com>
2016-08-30 20:59:02 +0200

    Finished Rock, Paper, Scissors game

--- Module 8.4/script.js
+++ Module 8.4/script.js
@@ -5,7 +5,7 @@ var player = {
 };
 
 var computer = {
-  name: 'computer',
+  name: 'Computer',
   score: 0,
   pick: ''
 };
@@ -20,4 +20,80 @@ var newGame = function(){
   round = 0;
   player.name = prompt("Jak się nazywasz?", "Jan Kowalski");
   document.getElementById("playerName").innerHTML = player.name;
-}
+  var buttons = document.getElementsByClassName("pick-button");
+  for(var i = 0; i < buttons.length; i++) {
+    buttons[i].style.visibility = 'visible';
+  };
+};
+
+var playerPick = function(pick){
+  player.pick=pick;
+  computerPick();
+  checkResult();
+  selectWinner();
+};
+
+var computerPick = function(){
+  switch (Math.floor(Math.random()*3)){
+    case 0:
+    computer.pick='rock';
+    break;
+    case 1:
+    computer.pick='paper';
+    break;
+    case 2:
+    computer.pick='scissors';
+    break;
+  }
+};
+
+var checkResult = function(){
+  var gameResult;
+  if (player.pick === computer.pick) {
+      gameResult = "Tie!";
+  }
+  else if (player.pick=='rock' && computer.pick=='paper') {
+    computer.score++;
+    gameResult="Computer wins!";
+  }
+  else if (player.pick=='rock' && computer.pick=='scissors') {
+    player.score++;
+    gameResult=player.name + " wins!";
+  }
+  else if (player.pick=='paper' && computer.pick=='rock') {
+    player.score++;
+    gameResult=player.name + " wins!";
+  }
+  else if (player.pick=='paper' && computer.pick=='scissors') {
+    computer.score++;
+    gameResult="Computer wins!";
+  }
+  else if (player.pick=='scissors' && computer.pick=='rock') {
+    computer.score++;
+    gameResult="Computer wins!";
+  }
+  else if (player.pick=='scissors' && computer.pick=='paper') {
+    player.score++;
+    gameResult=player.name + " wins!";
+  };
+  document.getElementById("playerPick").innerHTML = player.pick;
+  document.getElementById("computerPick").innerHTML = computer.pick;
+  document.getElementById("gameResult").innerHTML = gameResult;
+  document.getElementById("playerScore").innerHTML = player.score;
+  document.getElementById("computerScore").innerHTML = computer.score;
+};
+
+var selectWinner = function(){
+  if (player.score >= 10 || computer.score >= 10) {
+    if (player.score > computer.score) {
+      document.getElementById("roundResult").innerHTML = player.name + " wins the game!";
+    }
+    else {
+      document.getElementById("roundResult").innerHTML = "Computer wins the game!";
+    }
+    var buttons = document.getElementsByClassName("pick-button");
+    for(var i = 0; i < buttons.length; i++) {
+      buttons[i].style.visibility = 'hidden';
+    };
+  };
+};
