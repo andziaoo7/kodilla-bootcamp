3c4e30ed9e8fc986c2dcf59ad6a741be4ec39e35
andziaoo7 <a.kalamacka@gmail.com>
2016-09-06 21:16:44 +0200

    jQuery events task

---
+++ Module 9.3/script.js
@@ -0,0 +1,14 @@
+$(function(){
+  var carouselList = $("#carousel ul");
+  var slideLeft = function(){
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  };
+  var changeImage = function(){
+    carouselList.animate({"marginLeft": -400}, 500, slideLeft);
+  };
+  setInterval(function(){
+    changeImage()}, 3000);
+});
