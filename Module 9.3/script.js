42398cd4365deb4396a0a44cc5bbea2056eac6cf
Aneta Kałamacka <ankala@rinf.local>
2016-09-14 17:16:06 +0200

    Working images slider, without navigation arrows

--- Module 9.3/script.js
+++ Module 9.3/script.js
@@ -1,21 +1,21 @@
-// $(function(){
-//   var carouselList = $("#carousel ul");
-//   var slideLeft = function(){
-//     var firstItem = carouselList.find("li:first");
-//     var lastItem = carouselList.find("li:last");
-//     lastItem.after(firstItem);
-//     carouselList.css({marginLeft:0});
-//   };
-//   var changeImage = function(){
-//     carouselList.animate({"marginLeft": -400}, 500, slideLeft);
-//   };
-//   setInterval(function(){
-//     changeImage()}, 3000);
-// });
-$('.nextSlide').click(function() {
-    var currentSlide = $('.active-slide');
-    var nextSlide = currentSlide.next();
-
-    currentSlide.removeClass('active-slide');
-    nextSlide.addClass('active-slide');
-  });
+$(function(){
+  var carouselList = $("#carousel ul");
+  var slideLeft = function(){
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  };
+  var changeImage = function(){
+    carouselList.animate({"marginLeft": -400}, 500, slideLeft);
+  };
+  setInterval(function(){
+    changeImage()}, 3000);
+});
+// $('.nextSlide').click(function() {
+//     var currentSlide = $('.active-slide');
+//     var nextSlide = currentSlide.next();
+//
+//     currentSlide.removeClass('active-slide');
+//     nextSlide.addClass('active-slide');
+//   });
