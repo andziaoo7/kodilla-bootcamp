608866de24bc6e97fb90a40fe7271139db8ad584
andziaoo7 <a.kalamacka@gmail.com>
2016-09-18 13:57:26 +0200

    Slider with nav buttons

--- Module 9.3/script.js
+++ Module 9.3/script.js
@@ -1,21 +1,32 @@
-// $(function(){
-//   var carouselList = $("#carousel ul");
-//   var slideLeft = function(){
-//     var firstItem = carouselList.find("li:first");
-//     var lastItem = carouselList.find("li:last");
-//     lastItem.after(firstItem);
-//     carouselList.css({marginLeft:0});
-//   };
-//   var changeImage = function(){
-//     carouselList.animate({"marginLeft": -400}, 500, slideLeft);
-//   };
-//   setInterval(function(){
-//     changeImage()}, 3000);
-// });
-$('.nextSlide').click(function() {
-    var currentSlide = $('.active-slide');
-    var nextSlide = currentSlide.next();
+$(function(){
+  var carouselList = $("#carousel ul");
+  var slideLeft = function(){
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  };
+  var changeImage = function(){
+    carouselList.animate({"marginLeft": -400}, 500, slideLeft);
+  };
+  setInterval(function(){
+    changeImage()}, 3000);
+});
 
-    currentSlide.removeClass('active-slide');
-    nextSlide.addClass('active-slide');
-  });
+var $firstImage = $('li:first'); //czemu $('li:first', 'ul')? po co jeszcze daje się 'ul'?
+var $lastImage = $('li:last');
+$('#next').click(function(){
+  var $next;
+  var $active = $(".active");
+  $next = $active.next('li').length ? $active.next('li') : $firstImage;
+  $active.removeClass('active').fadeOut('fast');
+  $next.addClass('active').fadeIn('fast');
+});
+
+$('#prev').click(function(){
+  var $prev;
+  var $active = $(".active");
+  $prev = $active.prev('li').length ? $active.prev('li') : $lastImage;
+  $active.removeClass('active').fadeOut('fast');
+  $prev.addClass('active').fadeIn('fast');
+});
