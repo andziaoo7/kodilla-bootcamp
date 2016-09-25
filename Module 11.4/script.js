a47fc253f5b34f22e3b9137f21ffffb980cf68b1
andziaoo7 <a.kalamacka@gmail.com>
2016-09-25 10:21:38 +0200

    Cleaned JS code

--- Module 11.4/script.js
+++ Module 11.4/script.js
@@ -1,8 +1,9 @@
 var url = 'http://api.icndb.com/jokes/random';
+var xhr = new XMLHttpRequest();
+var paragraph = document.getElementById('joke');
 
 //Random joke on start page
 $(function(){
-  var xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.onload = function(){
     var response = JSON.parse(xhr.response);
@@ -17,11 +18,8 @@ var button = document.getElementById('get-joke');
 button.addEventListener('click', function() {
   getJoke();
 });
-var xhr = new XMLHttpRequest();
-var paragraph = document.getElementById('joke');
 
 function getJoke(){
-  // var xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.addEventListener('load', function(){
     var response = JSON.parse(xhr.response);
